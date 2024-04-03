f4a366e56d97f1f57d34c612d2086fb9096414d5
MuhammadKhairunnasNurdin <anasnurdin936@gmail.com>
2024-04-03 13:26:45 +0700

    Perf(Laravel-Ide-Helper): make barryvdh/laravel-ide-helper just running in development area

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (env('APP_ENV') == 'local') {
+            $this->app->register(IdeHelperServiceProvider::class);
+        }
     }
 
     /**
